2082d312a6b45813c12acc96039acb2270ee95d4
mobin212 <mobin.abdoli@gmail.com>
2022-08-29 20:21:02 +0430

    axios for login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,28 +1,52 @@
-import React from 'react'
-import {BrowserRouter as Router,Route,Link} from "react-router-dom";
+import axios from 'axios';
+import React, {useState} from 'react'
+import {Link} from "react-router-dom";
+import {instance} from '../axios'
 
 
 const Login = () => {
+  
+  let resinit = {
+    email:'',
+    password:'',
+    message:''
+  }
+  const [res, setRes] = useState(resinit);
+  const data = {
+    email:res.email,
+    password:res.password
+  }
+
+  const formSubmit = async (e) =>{
+    e.preventDefault()
+    try {
+      const resp = await axios.post(`http://localhost:8000/api/login`,data);
+      console.log(resp);
+  } catch (err) {
+      // Handle Error Here
+      console.error(err);
+  }
+    
+  }
+
+
   return (
     <div><br /><br />
-      <div className='row'>
-        <div className="jumbotron   col-lg-4 offset-lg-4">
+      <div className='row p-2'>
+        <div className="jumbotron p-2  col-lg-4 offset-lg-4">
             <h3 className='text-center'>Login Account</h3>
-            <form >
-                <div className="form-group">
+            <form onSubmit={formSubmit}>
+                <div className="form-group p-2">
                     <label htmlFor="exampleInputEmail1">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+                    <input type="email" value={res.email} onChange={(e)=>{setRes({...res, email: e.target.value})}} className="form-control" name='email' required aria-describedby="emailHelp" placeholder="Enter email"/>
                     <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
                 </div>
-                <div className="form-group">
+                <div className="form-group p-2">
                     <label htmlFor="exampleInputPassword1">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
+                    <input value={res.password} onChange={(e)=>{setRes({...res, password: e.target.value})}} type="password" className="form-control" name='password' id="exampleInputPassword1" placeholder="Password"/>
                 </div>
-                <div className="form-check">
-                    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-                    <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-                </div>
-                <button type="submit" className="btn btn-primary">Login</button>
+                
+                <button type="submit" className="btn btn-primary p-2 d-block">Login</button>
                 Forget Password <Link to='/forget'>Click Here</Link>
             </form>
         </div>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import axios from 'axios';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './common/Header';
+
+axios.defaults.baseURL = 'http://localhost:8000/api'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
